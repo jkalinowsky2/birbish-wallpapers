1a0a4c5e2355fb9192c27261e4dd8c1d4c191496
JK <jkalinowsky@gmail.com>
2025-11-21 08:41:34 -0500

    Added Droobins sticker

--- src/app/shop/products.ts
+++ src/app/shop/products.ts
@@ -45,7 +45,7 @@ export const STICKER_PRODUCTS: Product[] = [
         name: "I ❤️ MB Sticker",
         priceLabel: "$3.50",
         description: "I ❤️ MB sticker. 3 x 3 inches.",
-        image: "/assets/store/stickers/ilovembsticker.png",
+        image: "/assets/store/stickers/ilovembsticker-pat.png",
         priceId: "price_1SSNuf0n54kwZghJrl0bQDHP", //
         // priceId: "price_1SSOM914Mz115JiaQ6FwT8Lh", //test
     },
@@ -53,40 +53,48 @@ export const STICKER_PRODUCTS: Product[] = [
         id: "toobins-sticker",
         name: "Toobins Sticker",
         priceLabel: "$3.50",
-        description: "Toobins sticker. 2 x 3 inches",
+        description: "Toobins sticker. 2 x 2 inches",
         image: "/assets/store/stickers/toobinssticker.png",
         priceId: "price_1SSNva0n54kwZghJPTofWD8J", //
         // priceId: "price_1SSOLi14Mz115JiaUs96IXlh", //test
     },
-    {
-        id: "gm-stickerpack",
+    {id: "droobins-sticker",
+    name: "Droobins Sticker",
+    priceLabel: "$3.50",
+    description: "Droobins sticker. 2 x 2 inches",
+    image: "/assets/store/stickers/droobinssticker.png",
+    priceId: "price_1SVu470n54kwZghJnRSfukak", //
+   
+    },
+{
+    id: "gm-stickerpack",
         name: "Sticker Pack 1",
-        priceLabel: "$15.00",
-        description: "Birbs sticker pack - 5 birbish stickers",
-        image: "/assets/store/stickers/stickerpack1.png",
-        priceId: "price_1SSO0a0n54kwZghJjM2dgJBA", //price_1SSOIE14Mz115JiaVt3sskqi
+            priceLabel: "$15.00",
+                description: "Birbs sticker pack - 5 birbish stickers",
+                    image: "/assets/store/stickers/stickerpack1.png",
+                        priceId: "price_1SSO0a0n54kwZghJjM2dgJBA", //price_1SSOIE14Mz115JiaVt3sskqi
         // priceId: "price_1SSOIE14Mz115JiaVt3sskqi", //testing price ID
     },
 
-    {
-        id: "birb-euro-sticker",
+{
+    id: "birb-euro-sticker",
         name: "birb.  Euro Sticker",
-        priceLabel: "$5.00",
-        description: "birb. Euro sticker. 5 x 3 inches.",
-        image: "/assets/store/stickers/birbeurosticker.png",
-        priceId: "price_1SRxuC0n54kwZghJqVLWMmbI", //
+            priceLabel: "$5.00",
+                description: "birb. Euro sticker. 5 x 3 inches.",
+                    image: "/assets/store/stickers/birbeurosticker.png",
+                        priceId: "price_1SRxuC0n54kwZghJqVLWMmbI", //
         // priceId: "price_1STXcv14Mz115JiauynzDoBI", //test
 
     },
-    {
-        id: "birb-sticker-holo",
+{
+    id: "birb-sticker-holo",
         name: "Holographic Logo Sticker",
-        priceLabel: "$5.00",
-        description: "Free for Moonbirds holders w/ $10 purchase *",
-        image: "/assets/store/stickers/birblogosticker-holo.png",
-        priceId: "price_1STSql0n54kwZghJct2G53rB", //
-        // priceId: "price_1SSifY14Mz115JiadL5lXegX", // testing price ID
-        giftOnly: true,
+            priceLabel: "$5.00",
+                description: "Free for Moonbirds holders w/ $10 purchase *",
+                    image: "/assets/store/stickers/birblogosticker-holo.png",
+                        priceId: "price_1STSql0n54kwZghJct2G53rB", //
+                            // priceId: "price_1SSifY14Mz115JiadL5lXegX", // testing price ID
+                            giftOnly: true,
     },
 ];
 
@@ -127,28 +135,28 @@ export const STICKER_PRODUCTS: Product[] = [
 
 // 🔹 New transfer decals
 export const DECAL_PRODUCTS: Product[] = [
-  {
-    id: 'birblogodecal',
-    name: 'birb logo transfer',
-    priceId: 'price_1SUpO80n54kwZghJuaX7XhLx',        // Live
-    // priceId: 'price_1SUpEv14Mz115JiaLjZ5awBJ',        // Testing price ID
-    priceLabel: '$6.50',
-    image: '/assets/store/decals/birblogodecal.png',
-    description: 'Cream logo transfer sticker. 3 x 3 inches',
-  },
-//   {
-//     id: 'decal-logo-large',
-//     name: 'Logo Transfer Decal – Large',
-//     priceId: 'price_xxx',        // Stripe price for this decal
-//     priceLabel: '$15.00',
-//     image: '/assets/images/decals/logo-decal-large.png',
-//     description: 'Large transfer decal for laptops, cars, or cases. Approx 8" wide.',
-//   },
+    {
+        id: 'birblogodecal',
+        name: 'birb logo transfer',
+        priceId: 'price_1SUpO80n54kwZghJuaX7XhLx',        // Live
+        // priceId: 'price_1SUpEv14Mz115JiaLjZ5awBJ',        // Testing price ID
+        priceLabel: '$6.50',
+        image: '/assets/store/decals/birblogodecal.png',
+        description: 'Cream logo transfer sticker. 3 x 3 inches',
+    },
+    //   {
+    //     id: 'decal-logo-large',
+    //     name: 'Logo Transfer Decal – Large',
+    //     priceId: 'price_xxx',        // Stripe price for this decal
+    //     priceLabel: '$15.00',
+    //     image: '/assets/images/decals/logo-decal-large.png',
+    //     description: 'Large transfer decal for laptops, cars, or cases. Approx 8" wide.',
+    //   },
 ]
 
 // 🔹 Helper that includes *everything* for totals, etc.
 export const ALL_PRODUCTS: Product[] = [
-  ...STICKER_PRODUCTS,
-  ...DECAL_PRODUCTS,
+    ...STICKER_PRODUCTS,
+    ...DECAL_PRODUCTS,
 ]
 
